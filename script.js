4b39466f6a519fda8ecf1ed08208eea891d1b0bf
Maï Uchida <maiuchidakishaba@gmail.com>
2023-05-25 11:32:22 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggles = document.querySelectorAll('.text-toggle');
+
+toggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle('active')
+  })
+})
+
+
